07f8c80aeed60e0ab8eb8eb029c58b59d3099c55
Rudings <marius.svar@outlook.com>
2019-06-04 13:39:07 +0200

    System for initial game info almost completed

---
+++ Oslo Havn 1798/Assets/_Scripts/GameInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameInfo : MonoBehaviour
+{
+    public GameObject gameInfo;
+    public GameObject gameInfoText;
+
+    private bool gameInfoRead = false;
+
+    private void Start()
+    {
+        if(gameInfoRead == false)
+        {
+            gameInfo.SetActive(true);
+            gameInfoText.SetActive(true);
+        }
+        else
+        {
+            gameInfo.SetActive(false);
+            gameInfoText.SetActive(false);
+        }
+    }
+
+    public void GameInfoCompleted()
+    {
+        gameInfoRead = true;
+        gameInfo.SetActive(false);
+        gameInfoText.SetActive(false);
+    }
+}
