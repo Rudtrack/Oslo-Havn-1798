f8d6d81aa8f171daf599ddbdb7332f267f273ecf
Rudings <marius.svar@outlook.com>
2019-06-04 16:23:16 +0200

    About Us panel ready for content

--- Oslo Havn 1798/Assets/_Scripts/GameInfo.cs
+++ Oslo Havn 1798/Assets/_Scripts/GameInfo.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class GameInfo : MonoBehaviour
 {
     public GameObject gameInfo;
-    public GameObject gameInfoText;
+    //public GameObject gameInfoText;
+
+    public GameObject aboutUsPanel;
 
     private static bool gameInfoRead = false;
 
@@ -15,12 +17,12 @@ public class GameInfo : MonoBehaviour
         if(gameInfoRead == false)
         {
             gameInfo.SetActive(true);
-            gameInfoText.SetActive(true);
+            //gameInfoText.SetActive(true);
         }
         else
         {
             gameInfo.SetActive(false);
-            gameInfoText.SetActive(false);
+            //gameInfoText.SetActive(false);
         }
     }
 
@@ -28,6 +30,18 @@ public class GameInfo : MonoBehaviour
     {
         gameInfoRead = true;
         gameInfo.SetActive(false);
-        gameInfoText.SetActive(false);
+        //gameInfoText.SetActive(false);
+    }
+
+    public void ViewAboutUs()
+    {
+        if(aboutUsPanel.activeInHierarchy == false)
+        {
+            aboutUsPanel.SetActive(true);
+        }
+        else
+        {
+            aboutUsPanel.SetActive(false);
+        }
     }
 }
