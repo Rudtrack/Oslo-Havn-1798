0b7c02d74864d9fc1a658773235a8ff863bbe034
apelofff <joergenstolt@gmail.com>
2019-06-17 16:54:14 +0200

    Screen rotation

---
+++ Oslo Havn 1798/Assets/ScreenOrientation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenOrientation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 2)
+        {
+
+        Screen.autorotateToLandscapeLeft = true;
+
+        Screen.autorotateToLandscapeRight = true;
+
+        Screen.autorotateToPortrait = false;
+
+        Screen.autorotateToPortraitUpsideDown = false;
+        }
+        else
+        {
+            Screen.autorotateToLandscapeLeft = false;
+
+            Screen.autorotateToLandscapeRight = false;
+
+            Screen.autorotateToPortrait = true;
+
+            Screen.autorotateToPortraitUpsideDown = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
